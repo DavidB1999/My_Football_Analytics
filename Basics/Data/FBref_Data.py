138c782069ce09d231428119b2db4291e677b4ae
David Brinkjans <135713199+DavidB1999@users.noreply.github.com>
2023-12-01 13:53:54 +0100

    Revision of FBrefData

--- Basics/Data/FBref_Data.py
+++ Basics/Data/FBref_Data.py
@@ -17,7 +17,7 @@ def Scrape_Player_via_Link(URL):
     page_link = URL
     # page_link = 'https://fbref.com/en/players/b66315ae/Gabriel-Jesus'
     page = requests.get(page_link)
-    # parse it via BeautifoulSoup
+    # parse it via BeautifulSoup
     soup = BeautifulSoup(page.content, 'html.parser')
     # print(soup)
     # find player name
@@ -35,7 +35,7 @@ def Scrape_Player_via_Link(URL):
 
     # get the page via requests
     page = requests.get(full_url)
-    # parse it via BeautifoulSoup
+    # parse it via BeautifulSoup
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # find the players position according to
@@ -71,23 +71,15 @@ def Scrape_Player_via_Link(URL):
     rights = summary.find_all("td", class_='right')
     # the tds of class left endpoint [...] contain the csk with percentile values
     tooltips = summary.find_all("td", class_='left endpoint endpoint tooltip')
-    # the th of class right poptip endpoint endpoint contains variable names under data-tip
+    # the th of class right poptip endpoint contains variable names under data-tip
     poptips = summary.find_all("th", class_='right poptip endpoint endpoint')
 
     # for each element in that td get me the csk
-    values = []
-    for ele in rights:
-        csk = ele.get('csk')
-        values.append(csk)
+    values = [ele.get('csk') for ele in rights]
 
-    percentiles = []
-    for ele in tooltips:
-        csk = ele.get('csk')
-        percentiles.append(csk)
+    percentiles = [ele.get('csk') for ele in tooltips]
 
-    variables = []
-    for ele in poptips:
-        variables.append(ele.string)
+    variables = [ele.string for ele in poptips]
 
     while None in values:
         # removing None from list using remove method
@@ -102,15 +94,10 @@ def Scrape_Player_via_Link(URL):
         else:
             duplicate_index.append(var_number)
 
-    par_values = []
-    for v_num, v in enumerate(values):
-        if v_num not in duplicate_index:
-            par_values.append(v)
-
-    par_percentiles = []
-    for p_num, p in enumerate(percentiles):
-        if p_num not in duplicate_index:
-            par_percentiles.append(p)
+    # get all values
+    par_values = [v for v_num, v in enumerate(values) if v_num not in duplicate_index]
+    # get all percentile ranks
+    par_percentiles = [p for p_num, p in enumerate(percentiles) if p_num not in duplicate_index]
 
     # change to numeric!
     par_percentiles_int = [int(x) for x in par_percentiles]
@@ -119,6 +106,7 @@ def Scrape_Player_via_Link(URL):
     df = pd.DataFrame(zip(parameters, par_values_int, par_percentiles_int),
                       columns=['Variables', 'Values', 'Percentiles'])
     return df, name, position
+
 
 # -----------------------------------------------------
 # same function adapted for streamlit i.e. split in two
@@ -130,12 +118,13 @@ def Scrape_Player_via_Link_st1(URL):
     page_link = URL
     # page_link = 'https://fbref.com/en/players/b66315ae/Gabriel-Jesus'
     page = requests.get(page_link)
-    # parse it via BeautifoulSoup
+    # parse it via BeautifulSoup
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # find player name
     script = (soup.find_all(type="application/ld+json"))
     data = [json.loads(x.string) for x in script]
+
     for d in data:
         name = (d['name'])
 
@@ -148,7 +137,7 @@ def Scrape_Player_via_Link_st1(URL):
 
     # get the page via requests
     page = requests.get(full_url)
-    # parse it via BeautifoulSoup
+    # parse it via BeautifulSoup
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # find the players position according to
@@ -182,19 +171,11 @@ def Scrape_Player_via_Link_st2(soup, selected_position, unique_positions, positi
     poptips = summary.find_all("th", class_='right poptip endpoint endpoint')
 
     # for each element in that td get me the csk
-    values = []
-    for ele in rights:
-        csk = ele.get('csk')
-        values.append(csk)
+    values = [ele.get('csk') for ele in rights]
 
-    percentiles = []
-    for ele in tooltips:
-        csk = ele.get('csk')
-        percentiles.append(csk)
+    percentiles = [ele.get('csk') for ele in tooltips]
 
-    variables = []
-    for ele in poptips:
-        variables.append(ele.string)
+    variables = [ele.string for ele in poptips]
 
     while None in values:
         # removing None from list using remove method
@@ -209,16 +190,10 @@ def Scrape_Player_via_Link_st2(soup, selected_position, unique_positions, positi
         else:
             duplicate_index.append(var_number)
 
-    par_values = []
-    for v_num, v in enumerate(values):
-        if v_num not in duplicate_index:
-            par_values.append(v)
-
-    par_percentiles = []
-    for p_num, p in enumerate(percentiles):
-        if p_num not in duplicate_index:
-            par_percentiles.append(p)
-
+    # get all values
+    par_values = [v for v_num, v in enumerate(values) if v_num not in duplicate_index]
+    # get all percentile ranks
+    par_percentiles = [p for p_num, p in enumerate(percentiles) if p_num not in duplicate_index]
 
     # change to numeric!
     par_percentiles_int = [int(x) for x in par_percentiles]
@@ -228,6 +203,7 @@ def Scrape_Player_via_Link_st2(soup, selected_position, unique_positions, positi
                       columns=['Variables', 'Values', 'Percentiles'])
     return df, position
 
+
 # ------------------------------------------------------------------------------------------------------
 # Had the idea in mind to change the function above, so that it does not require a correct URL
 # Instead entering a players name should result in the first result of the search on FBref being scraped
@@ -235,10 +211,3 @@ def Scrape_Player_via_Link_st2(soup, selected_position, unique_positions, positi
 def Scrape_Player_via_Search(searchterm):
     # this might be included later
     print('Not yet available!')
-
-
-
-
-
-
-
